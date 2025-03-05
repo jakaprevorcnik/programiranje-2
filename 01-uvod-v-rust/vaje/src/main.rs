1b5216643770477931046b54504df9cacedb5bd3
Jaka Prevorcnik <jp3307@student.uni-lj.si>
2025-03-05 08:41:40 +0100

    vaje01

--- 01-uvod-v-rust/vaje/src/main.rs
+++ 01-uvod-v-rust/vaje/src/main.rs
@@ -7,126 +7,170 @@ use core::panic;
 /// Napišite funkcijo `fib`, ki sprejme začetna člena fibbonacijevega zaporedja, število `n` in vrne `n`-ti člen zaporedja
 
 fn fib(a0: u32, a1: u32, n: u32) -> u32 {
-    panic!("Not implemented");
+    let mut a = a0;
+    let mut b  = a1;
+    for _ in 0..n {
+        (a, b) = (b, a + b)
+    }
+    return a
 }
 
-/// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 
-/// Napišite funkcijo `je_prestopno`, ki za podano leto preveri, ali je prestopno
+// Napišite funkcijo `je_prestopno`, ki za podano leto preveri, ali je prestopno
+fn je_prestopno1 (leto: u32) -> bool {
+    if leto % 4 == 0 {
+        if leto % 100 == 0 {
+            if leto % 400 == 0 {
+                return true}
+            return false}
+        return true}
+    return false}
 
-/// ------------------------------------------------------------------------------------------------
+fn je_prestopno2 (leto: u32) -> bool{ //lepse izgleda
+    (leto % 4 == 0 && leto % 100 != 0) || (leto % 400 == 0) 
+}
+// ------------------------------------------------------------------------------------------------
 
-/// Napišite funkcijo `je_veljaven_datum(datum: Date) -> bool`, ki preveri, ali je datum veljaven
+// Napišite funkcijo `je_veljaven_datum(datum: Date) -> bool`, ki preveri, ali je datum veljaven
 
-// Dan, mesec, leto
+ // Dan, mesec, leto
 type Date = (u32, u32, u32);
-
-/// ------------------------------------------------------------------------------------------------
-
-/// Napišite funkcijo `iteracija(mut start: u32, fun: fn(u32) -> u32, cond: fn(u32) -> bool) -> u32`, ki sprejme iteracijsko funkcijo, zaustavitveni pogoj in začetno vrednost.
-/// Iteracijsko funkcijo zaporedoma uporablja, dokler za rezultat ne velja zaustavitveni pogoj, in vrne prvi rezultat, ki zadošča zaustavitvenemu pogoju.
-
-fn iteracija(mut start: u32, fun: fn(u32) -> u32, cond: fn(u32) -> bool) -> u32 {
-    panic!("Not implemented");
-}
-
-/// ------------------------------------------------------------------------------------------------
-
-/// Napišite funkcijo, ki izračuna ničlo zvezne funkcije s pomočjo bisekcije.
-/// Postopek bisekcije je sledeč:
-/// 1. Izberemo interval [a, b], kjer je f(a) * f(b) < 0
-/// 2. Izračunamo sredino intervala c = (a + b) / 2
-/// 3. Če je |f(c)| < prec ali je dolžina intervala manjša od določene natančnosti, vrnemo c
-/// 4. Če ni, izberemo nov interval [a, b] glede na predznak f(c)
-/// 5. Ponavljamo korake 2-4
-
-fn bisekcija(mut a: f64, mut b: f64, fun: fn(f64) -> f64, prec: f64) -> f64 {
-    panic!("Not implemented");
-}
-
-/// ------------------------------------------------------------------------------------------------
-
-/// Popravite igro ugibanja iz prejšnje naloge, da bo delovala sledeče
-/// Uporabnika sprašujemo po novi številki, vse dokler so števila, ki jih vpisuje del nekega aritmetičnega zaporedja
-/// Če uporabnik vpiše neveljavno število to ni napaka, program za pogoj aritmetičnega zaporedja upošteva samo veljavno vpisana števila.
-
-fn guessing_game() {
-    panic!("Not implemented");
-}
-
-/// ------------------------------------------------------------------------------------------------
-/// Napišite funkcijo `fn mat_mul(a: [[u32; 2]; 2], b: [[u32; 2]; 2]) -> [[u32; 2]; 2]`, ki matriki `a` in `b` zmnoži in vrne rezultat
-
-fn mat_mul(a: [[u32; 2]; 2], b: [[u32; 2]; 2]) -> [[u32; 2]; 2] {
-    panic!("Not implemented");
-}
-
-/// ------------------------------------------------------------------------------------------------
-/// Napišite funkcijo `ordered`, ki sprejme tabelo števil in vrne `true`, če so števila urejena (padajoče ali naraščajoče) in `false` sicer.
-
-fn ordered(arr: &[u32]) -> bool {
-    panic!("Not implemented");
-}
-
-fn vsebuje<T : PartialEq>(v: &Vec<T>, x : &T) -> bool {
-    for y in v {
-      if x == y {
-        return true
-      }
+fn je_veljaven_datum ((dan, mesec, leto): Date) -> bool{
+    if dan == 0 || mesec == 0 || leto == 0 || mesec > 12 {
+        return false;
     }
-    return false
-}
-
-/// ------------------------------------------------------------------------------------------------
-/// Hitro potenciranje
-/// Napišite funkcijo `fn pow(mut x: u32, mut n: u32) -> u32`, ki izračuna `x` na potenco `n` v času O(log n)
-/// Hitro potenciranje izgleda tako:
-/// 1. Če je `n` sodo, potem je `x^n = (x^(n/2))^2`
-/// 2. Če je `n` liho, potem je `x^n = (x^2)^(n/2)`
-/// 3. Če je `n = 0`, potem je `x^n = 1`
-
-/// ------------------------------------------------------------------------------------------------
-/// Prepišite hitro potenciranje v iterativno obliko
-
-/// ------------------------------------------------------------------------------------------------
-/// Hitro potenciranje deluje tudi, če nas zanima samo ostanek po deljenju z nekim številom `m`
-/// Napišite funkcijo `fn pow_mod(mut x: u32, mut n: u32, m: u32) -> u32`, ki izračuna `x` na potenco `n` in vrne ostanek po deljenju z `m`
-/// Postopek je enak, le da pri vsakem izračunu vrnemo ostanek pri deljenju z `m`
-
-/// ------------------------------------------------------------------------------------------------
-/// Urejanje z izbiranjem
-/// Napišite funkcijo `fn selection_sort(arr: &mut [u32])`, ki uredi tabelo `arr` z uporabo algoritma urejanja z izbiranjem
-
-fn selection_sort(arr: &mut [u32]) {}
-
-/// ------------------------------------------------------------------------------------------------
-/// Napišite program, ki izpiše piramido višine `n` iz zvezdic
-
-fn pyramid(n: u32) {
-    panic!("Not implemented");
-}
-
-/// ------------------------------------------------------------------------------------------------
-/// Napišite program, ki izpiše piramido črk angleške abecede višine `n`, lahkom predpostavite, da bo n največ 26.
-///      A
-///    A B A
-///   A B C B A
-/// A B C D C B A
-/// Napišite funkcijo `fn selection_sort(mut arr: [u32])`, ki uredi tabelo `arr` z uporabo algoritma urejanja z izbiranjem
-
-fn main() {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = main();
-        assert_eq!(result, ());
+    else if mesec == 2 {
+        if je_prestopno1(leto) {
+            return dan <= 29;  // Correctly checks for leap year
+        } else {
+            return dan <= 28;  // Correctly checks for non-leap year
+        }
+    } else if [1,3,5,7,8,10,12].contains(&mesec) {
+        if dan <= 31 {
+            return true;
+        } return false;
+    } else if dan <= 30 {
+        return true;
     }
-
-    #[test]
-    fn test_fib() {
-    }
+    return false;
 }
+ // ------------------------------------------------------------------------------------------------
+
+ // Napišite funkcijo `iteracija(mut start: u32, fun: fn(u32) -> u32, cond: fn(u32) -> bool) -> u32`, ki sprejme iteracijsko funkcijo, zaustavitveni pogoj in začetno vrednost.
+ // Iteracijsko funkcijo zaporedoma uporablja, dokler za rezultat ne velja zaustavitveni pogoj, in vrne prvi rezultat, ki zadošča zaustavitvenemu pogoju
+ fn iteracija(mut start: u32, fun: fn(u32) -> u32, cond: fn(u32) -> bool) -> u32 {
+     let mut s = start;
+     while !cond(s) {
+        s = fun(s);
+     }
+     return s
+ }
+
+// ------------------------------------------------------------------------------------------------
+
+// /// Napišite funkcijo, ki izračuna ničlo zvezne funkcije s pomočjo bisekcije.
+// /// Postopek bisekcije je sledeč:
+// /// 1. Izberemo interval [a, b], kjer je f(a) * f(b) < 0
+// /// 2. Izračunamo sredino intervala c = (a + b) / 2
+// /// 3. Če je |f(c)| < prec ali je dolžina intervala manjša od določene natančnosti, vrnemo c
+// /// 4. Če ni, izberemo nov interval [a, b] glede na predznak f(c)
+// /// 5. Ponavljamo korake 2-4
+
+ fn bisekcija(mut a: f64, mut b: f64, fun: fn(f64) -> f64, prec: f64) -> f64 {
+     let mut c = (a + b)/ 2;
+     while (fun(c)).abs() >= prec {
+        if fun (c) > 0 {
+            
+        }
+     }
+ }
+
+// /// ------------------------------------------------------------------------------------------------
+
+// /// Popravite igro ugibanja iz prejšnje naloge, da bo delovala sledeče
+// /// Uporabnika sprašujemo po novi številki, vse dokler so števila, ki jih vpisuje del nekega aritmetičnega zaporedja
+// /// Če uporabnik vpiše neveljavno število to ni napaka, program za pogoj aritmetičnega zaporedja upošteva samo veljavno vpisana števila.
+
+// fn guessing_game() {
+//     panic!("Not implemented");
+// }
+
+// /// ------------------------------------------------------------------------------------------------
+// /// Napišite funkcijo `fn mat_mul(a: [[u32; 2]; 2], b: [[u32; 2]; 2]) -> [[u32; 2]; 2]`, ki matriki `a` in `b` zmnoži in vrne rezultat
+
+// fn mat_mul(a: [[u32; 2]; 2], b: [[u32; 2]; 2]) -> [[u32; 2]; 2] {
+//     panic!("Not implemented");
+// }
+
+// /// Napišite funkcijo `ordered`, ki sprejme tabelo števil in vrne `true`, če so števila urejena (padajoče ali naraščajoče) in `false` sicer.
+
+// fn ordered(arr: &[u32]) -> bool {
+//     panic!("Not implemented");
+// }
+
+// fn vsebuje<T : PartialEq>(v: &Vec<T>, x : &T) -> bool {
+//     for y in v {
+//       if x == y {
+//         return true
+//       }
+//     }
+//     return false
+// }
+
+// /// ------------------------------------------------------------------------------------------------
+// /// Hitro potenciranje
+// /// Napišite funkcijo `fn pow(mut x: u32, mut n: u32) -> u32`, ki izračuna `x` na potenco `n` v času O(log n)
+// /// Hitro potenciranje izgleda tako:
+// /// 1. Če je `n` sodo, potem je `x^n = (x^(n/2))^2`
+// /// 2. Če je `n` liho, potem je `x^n = (x^2)^(n/2)`
+// /// 3. Če je `n = 0`, potem je `x^n = 1`
+
+// /// ------------------------------------------------------------------------------------------------
+// /// Prepišite hitro potenciranje v iterativno obliko
+
+// /// ------------------------------------------------------------------------------------------------
+// /// Hitro potenciranje deluje tudi, če nas zanima samo ostanek po deljenju z nekim številom `m`
+// /// Napišite funkcijo `fn pow_mod(mut x: u32, mut n: u32, m: u32) -> u32`, ki izračuna `x` na potenco `n` in vrne ostanek po deljenju z `m`
+// /// Postopek je enak, le da pri vsakem izračunu vrnemo ostanek pri deljenju z `m`
+
+// /// ------------------------------------------------------------------------------------------------
+// /// Urejanje z izbiranjem
+// /// Napišite funkcijo `fn selection_sort(arr: &mut [u32])`, ki uredi tabelo `arr` z uporabo algoritma urejanja z izbiranjem
+
+// fn selection_sort(arr: &mut [u32]) {}
+
+// /// ------------------------------------------------------------------------------------------------
+// /// Napišite program, ki izpiše piramido višine `n` iz zvezdic
+
+// fn pyramid(n: u32) {
+//     panic!("Not implemented");
+// }
+
+// /// ------------------------------------------------------------------------------------------------
+// /// Napišite program, ki izpiše piramido črk angleške abecede višine `n`, lahkom predpostavite, da bo n največ 26.
+// ///      A
+// ///    A B A
+// ///   A B C B A
+// /// A B C D C B A
+// /// Napišite funkcijo `fn selection_sort(mut arr: [u32])`, ki uredi tabelo `arr` z uporabo algoritma urejanja z izbiranjem
+
+fn main() {
+    let date1: Date = (29,2,2024);
+    println!("je/ni veljaven {}", je_veljaven_datum(date1));
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = main();
+//         assert_eq!(result, ());
+//     }
+
+//     #[test]
+//     fn test_fib() {
+//     }
+// }
--- 02-lastnistvo/vaje/src/main.rs
+++ 02-lastnistvo/vaje/src/main.rs
@@ -1,205 +1,207 @@
 use std::time::{Duration, Instant};
 
-fn time_it<F: FnOnce() -> R, R>(f: F) -> Duration {
-    let start = Instant::now();
-    f();
-    start.elapsed()
-}
+// fn time_it<F: FnOnce() -> R, R>(f: F) -> Duration {
+//     let start = Instant::now();
+//     f();
+//     start.elapsed()
+// }
 
-fn on_stack() {
-    // Narišite shemo spreminjanja sklada in kopice
-    // Za vsako vrstico napiši, kolikokrat se v pomnilniku pojavi 13?
-    let mut a = [13; 100];
-    let mut b = a;
-    let q = String::from("13");
-    println!("{}", q);
-    let r = q;
-    let p = &r;
-    a[0] = 1;
-    {
-        let c = &b;
-        println!("{}", c[0]);
-    }
-    println!("{}", b[0]);
-    println!("{}", a[0]);
-    println!("{}", p);
-    println!("{}", r);
-    // println!("{}", q); // Razloži, zakaj to ne deluje
-}
+// fn on_stack() {
+//     // Narišite shemo spreminjanja sklada in kopice
+//     // Za vsako vrstico napiši, kolikokrat se v pomnilniku pojavi 13?
+//     let mut a = [13; 100];
+//     let mut b = a;
+//     let q = String::from("13");
+//     println!("{}", q);
+//     let r = q;
+//     let p = &r;
+//     a[0] = 1;
+//     {
+//         let c = &b;
+//         println!("{}", c[0]);
+//     }
+//     println!("{}", b[0]);
+//     println!("{}", a[0]);
+//     println!("{}", p);
+//     println!("{}", r);
+//     // println!("{}", q); // Razloži, zakaj to ne deluje
+// }
 
-/// Napišite funkcijo `swap`, ki zamenja vrednosti dveh celoštevilskih spremenljivk.
-fn test_swap() {
-    // V spremenljivko `a` shranite vrednost 13, v spremenljivko `b` pa vrednost 42.
+// /// Napišite funkcijo `swap`, ki zamenja vrednosti dveh celoštevilskih spremenljivk.
+// fn test_swap() {
+//     // V spremenljivko `a` shranite vrednost 13, v spremenljivko `b` pa vrednost 42.
 
-    // println!("a: {}, b: {}", a, b);
-    // Izpiše `a: 13, b: 42`.
+//     // println!("a: {}, b: {}", a, b);
+//     // Izpiše `a: 13, b: 42`.
 
-    // Naredite swap s pomočjo pomožne funkcije `swap`.
-    // ...
-    //
+//     // Naredite swap s pomočjo pomožne funkcije `swap`.
+//     // ...
+//     //
 
-    // println!("a: {}, b: {}", a, b);
-    // Izpiše `a: 42, b: 13`.
-}
+//     // println!("a: {}, b: {}", a, b);
+//     // Izpiše `a: 42, b: 13`.
+// }
 
-/// Popravite zakomentiran del spodnje funkcije, da bo deloval
-fn str_own() {
-    // let x = String::from("Hello world");
-    // let y = x
-    // println!("{}, {}", x, y);
-}
+// /// Popravite zakomentiran del spodnje funkcije, da bo deloval
+ fn str_own() {
+      let x = "Hello world"; // -samo "Helo world" ni spremenljiva za razliko od String.from("Hello world")
+      let y = &x;  // &-nam da referenco. y sedaj ni prevzel lastnistva (samo kaze na x)
+      println!("{}, {}", x, y);
+ }
 
-/// Popravite brez uporabe funkcije `clone`
-/// Namig: sklad in kopiranje na skladu - kodo lahko spremenite
-fn str_own2() {
-    // let x = (1, 2, (), String::from("Hello world"));
-    // let y = x;
-    // println!("{:?}, {:?}", x, y);
-}
+ /// Popravite brez uporabe funkcije `clone`
+ /// Namig: sklad in kopiranje na skladu - kodo lahko spremenite
+ fn str_own2() {
+     // let x = (1, 2, (), String::from("Hello world")); //tukaj bi morali napisat samo "Hello world"
+     // let y = x;
+     // println!("{:?}, {:?}", x, y);
+ }
 
-/// Popravite spodnji dve funkciji, da bosta delovali
+// /// Popravite spodnji dve funkciji, da bosta delovali
 
-fn wrong() {
-    // let s = String::from("Hello World");
-    // print_str(s);
-    // println!("{}", s);
-}
+ fn wrong() {
+     // let s = String::from("Hello World");
+     // print_str(&s); //lahko bi bil tudi s.clone v argumentu vendar to vzame vec casa
+     // println!("{}", s);
+ }
 
-fn print_str(s: String) {
+fn print_str(s: &String) {
     println!("{}", s)
 }
 
-/// ------------------------------------------------------------------------------------------------
-/// Popravite spodnjo funkcijo, da bo delovala
-fn fn1() {
-    // let s = String::from("Hello ");
+// /// ------------------------------------------------------------------------------------------------
+// /// Popravite spodnjo funkcijo, da bo delovala
+ fn fn1() {
+     // let s = String::from("Hello ");
 
-    // let s1 = s;
+    // let s1 = s; //s1 ni mut zato moramo dat let mut s1 = s. !ne rabi bit zaradi tega ze s mut!
 
     // s1.push_str("World!");
 
-    // println!("Success!");
-}
+     // println!("Success!");
+ }
 
-/// ------------------------------------------------------------------------------------------------
-/// Popravite spodnjo funkcijo, da bo delovala
+// /// ------------------------------------------------------------------------------------------------
+// /// Popravite spodnjo funkcijo, da bo delovala
 
-fn fn2() {
-    // let x = Box::new(5);
+// fn fn2() {
+//     // let x = Box::new(5);
 
-    // // Popravite zgolj tukaj vmes
+//     // // Popravite zgolj tukaj vmes
 
-    // //
-    // *y = 4;
+//     // //
+//     // *y = 4;
 
-    // assert_eq!(*x, 5);
+//     // assert_eq!(*x, 5); //assert_eq ce bo enako ne bo naredil nic drugace se sesuje
 
-    // println!("Success!");
-}
+//     // println!("Success!");
+// }
 
-/// ------------------------------------------------------------------------------------------------
+// /// ------------------------------------------------------------------------------------------------
 
-fn fn3() {
-    let t = (
-        String::from("hello"),
-        String::from("world"),
-        String::from("!"),
-    );
+// fn fn3() {
+     let t = (
+         String::from("hello"),
+         String::from("world"),
+         String::from("!"),
+     );
 
-    let _s = t.1;
+     let _s = t.1; //_s prevzame lastnstvo nad prvo komponento v touplu
 
-    // Izpišite čim večji del t-ja.
-    println!("????????");
-}
+     // Izpišite čim večji del t-ja.
+     println!("{}{}", t.0,t.2); // t.1 ne moremo izpisat ker ima s lastnistvo nad njim
+ }
 
-/// ------------------------------------------------------------------------------------------------
+// /// ------------------------------------------------------------------------------------------------
 
-fn fn4() {
-    let x = 5;
-    // Izpišite naslov spremenljivke x
-}
+ fn fn4() {
+     let x = 5;
+     // Izpišite naslov spremenljivke x
+    println!("{:p}",&x) //ukaz da vidimo kje v pomnilniku se nahaja x
+ }
 
-/// ------------------------------------------------------------------------------------------------
+// /// ------------------------------------------------------------------------------------------------
 
-fn fn5() {
-    let x = 13;
-    let y = &x;
+ fn fn5() {
+     let x = 13;
+     let y = &x
+     // Popravite spodnjo vrstico, da bo bo enakost držala
+      assert_eq!(13, *y); // * odpakira y
 
-    // Popravite spodnjo vrstico, da bo bo enakost držala
-    // assert_eq!(13, y);
-}
+ }
 
-/// ------------------------------------------------------------------------------------------------
+// /// ------------------------------------------------------------------------------------------------
 
-/// Popravite spodnjo funkcijo, funkcija `helper` se mora poklicati čim bolj učinkovito.
-fn fn6() {
-    let mut s = String::from("hello, ");
+ /// Popravite spodnjo funkcijo, funkcija `helper` se mora poklicati čim bolj učinkovito.
+ fn fn6() {
+     let mut s = String::from("hello, ");
 
-    // helper(s);
+     helper(&s); //dodas referenco
 
-    println!("Success!");
-}
+     println!("Success!");
+// }
 
-// Te funkcije ne spreminjajte
-fn helper(s: &String) {}
+ // Te funkcije ne spreminjajte
+ fn helper(s: &String) {}
 
-/// ------------------------------------------------------------------------------------------------
+// /// ------------------------------------------------------------------------------------------------
 
-/// Popravite spodnjo funkcijo, funkcija `helper2` se mora poklicati čim bolj učinkovito.
-fn fn7() {
-    let mut s = String::from("hello, ");
+// /// Popravite spodnjo funkcijo, funkcija `helper2` se mora poklicati čim bolj učinkovito.
+ fn fn7() {
+     let mut s = String::from("hello, ");
 
-    // helper2(s);
+     helper2(&mut s2); //sposodimo si mutable
 
-    println!("Success!");
-}
-// Te funkcije ne spreminjajte
-fn helper2(s: &mut String) {
-    s.push_str("world")
-}
+     println!("Success!");
+// }
+// // Te funkcije ne spreminjajte
+// fn helper2(s: &mut String) {
+//     s.push_str("world")
+// }
 
-/// ------------------------------------------------------------------------------------------------
+// /// ------------------------------------------------------------------------------------------------
 
-/// Pojasnite, zakaj spodnja koda ne deluje
-fn fn8() {
-    // let mut s = String::from("hello, ");
+// /// Pojasnite, zakaj spodnja koda ne deluje
+ fn fn8() {
+      let mut s = String::from("hello, ");
 
-    // let p = &mut s;
+      let p = &mut s;
 
-    // p.push_str("world");
+      p.push_str("world"); //p sprejme sebe kot argument zato rabimo referenco
 
-    // println!("Success! {}", p);
-    // println!("Success! {}", s);
-    // p.push_str("!");
-}
+      println!("Success! {}", p); 
+      println!("Success! {}", s); // ne bo delal ker s ni mut problem je da si ga nemoremo sposodit vec kot enkrat
+      p.push_str("!"); 
+ }
 
-/// ------------------------------------------------------------------------------------------------
-/// Pojasnite, zakaj spodnja koda ne deluje in jo popravite
-/// Pojasnite tudi zakaj je popravek ok
+// /// ------------------------------------------------------------------------------------------------
+// /// Pojasnite, zakaj spodnja koda ne deluje in jo popravite
+// /// Pojasnite tudi zakaj je popravek ok
 
-fn fn9() {
-    // let mut s = String::from("hello");
+ fn fn9() {
+      let mut s = String::from("hello");
 
-    // let r1 = &mut s;
-    // let r2 = &mut s;
+      let r1 = &mut s; //tukaj smo si isto dvakrat sposodli mut
+      let r2 = &mut s; //moramo si sposodit al nickar ali pa samo enkrat
+      // lahko bi tudi si prvi sposodli mut s in drugic &r1
 
-    // println!("{}, {}", r1, r2);
+     println!("{}, {}", r1, r2);
 
-    // println!("Success!");
-}
+      println!("Success!");
+ }
 
-/// ------------------------------------------------------------------------------------------------
-fn fn10() {
-    // // Popravite spodnjo vrstico
-    // let s = String::from("hello, ");
+// /// ------------------------------------------------------------------------------------------------
+ fn fn10() {
+     // // Popravite spodnjo vrstico
+      let mut s = String::from("hello, "); //rabmo s mut ker si ga spodaj sposodimo mut
 
-    // helper3(&mut s);
+     helper3(&mut s);
 
-    // println!("Success!");
-}
+      println!("Success!");
+ }
 
-fn helper3(s: &mut String) {}
+ fn helper3(s: &mut String) {}
 
-/// ------------------------------------------------------------------------------------------------
+// /// ------------------------------------------------------------------------------------------------
 
-fn main() {}
+ fn main() {}
